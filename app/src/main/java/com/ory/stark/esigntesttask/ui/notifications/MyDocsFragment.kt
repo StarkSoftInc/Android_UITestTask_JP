0d90c682a5c1040061f3c392dec6fd04812a222f
Oryna Starkina <oryna.stark@gmail.com>
2022-01-12 18:25:16 +0200

    added bottom navigation

--- app/src/main/java/com/ory/stark/esigntesttask/ui/notifications/MyDocsFragment.kt
+++ app/src/main/java/com/ory/stark/esigntesttask/ui/notifications/MyDocsFragment.kt
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.ory.stark.esigntesttask.R
-import com.ory.stark.esigntesttask.databinding.FragmentNotificationsBinding
+import com.ory.stark.esigntesttask.databinding.FragmentMyDocsBinding
 
-class NotificationsFragment : Fragment() {
+class MyDocsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var myDocsViewModel: MyDocsViewModel
+    private var _binding: FragmentMyDocsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        myDocsViewModel =
+            ViewModelProvider(this).get(MyDocsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMyDocsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        myDocsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/ory/stark/esigntesttask/ui/notifications/MyDocsViewModel.kt
+++ app/src/main/java/com/ory/stark/esigntesttask/ui/notifications/MyDocsViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class MyDocsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is My Docs"
     }
     val text: LiveData<String> = _text
 }
