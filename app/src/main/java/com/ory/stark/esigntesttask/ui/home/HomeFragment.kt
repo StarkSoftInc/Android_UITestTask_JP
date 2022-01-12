f44714e8f5c9a982c7c9967c4226a715d084c7a2
Oryna Starkina <oryna.stark@gmail.com>
2022-01-12 17:47:42 +0200

    added hiding of sign requests added dropdown icon

--- app/src/main/java/com/ory/stark/esigntesttask/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ory/stark/esigntesttask/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.ory.stark.esigntesttask.ui.home
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -33,11 +35,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        _binding?.homePageUpgradeBtn?.setOnClickListener {
+            crossfadeSignRequestsView()
+        }
 
         _binding?.homePageRecentActions?.run {
             hasFixedSize()
@@ -58,6 +64,28 @@ class HomeFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun crossfadeSignRequestsView() {
+
+        val shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+        binding.homePageSignRequests.apply {
+            // Set the content view to 0% opacity but visible, so that it is visible
+            // (but fully transparent) during the animation.
+            alpha = 1f
+            visibility = View.VISIBLE
+
+            // Animate the content view to 100% opacity, and clear any animation
+            // listener set on the view.
+            animate()
+                .alpha(0f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        binding.homePageSignRequests.visibility = View.GONE
+                    }
+                })
+        }
     }
 
     override fun onDestroyView() {
